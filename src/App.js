30aff7b03a935cd20e24a8db77107cad90366c98
Alek Angelov <alek@pabau.com>
2019-04-10 12:57:59 +0200

    💯

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./assets/styles/main.sass";
+import Epitaph from "epitaphjs";
+import { TimelineLite, Power4 } from "gsap";
 
 class App extends Component {
+  timeline = new TimelineLite({ paused: true });
+  componentDidMount() {
+    const heading = new Epitaph(this.heading, { className: "letter" });
+    const subheading = new Epitaph(this.subheading, { className: "letter" });
+    this.timeline.staggerFrom(
+      this.heading.childNodes,
+      3,
+      {
+        opacity: 0,
+        y: 200,
+        ease: Power4.easeOut
+      },
+      0.2
+    );
+    this.timeline.staggerFrom(
+      this.subheading.childNodes,
+      4,
+      {
+        opacity: 0,
+        y: 100,
+        rotationZ: "60deg",
+        ease: Power4.easeOut
+      },
+      0.05,
+      "-=3"
+    );
+    setTimeout(() => {
+      this.timeline.play();
+    }, 500);
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <>
+        <div className="wrapper">
+          <h1
+            ref={ref => {
+              this.heading = ref;
+            }}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
+            Epitaph
+          </h1>
+          <h4
+            ref={ref => {
+              this.subheading = ref;
+            }}
+          >
+            A text splitter plugin that does more
+          </h4>
+        </div>
+        <div className="footer">
+          <ul className="footer-menu">
+            <li>
+              <a href="https://github.com/alekangelov/epitaphjs#readme">
+                Github
+              </a>
+            </li>
+            <li>
+              <a href="https://www.npmjs.com/package/epitaphjs">NPM</a>
+            </li>
+            <li>
+              <a>
+                <a href="#">Documentation (soon)</a>
+              </a>
+            </li>
+          </ul>
+          <div className="footer-by">
+            <p>
+              Made by <a href="http://alekangelov.com">Alek Angelov</a>
+            </p>
+          </div>
+        </div>
+      </>
     );
   }
 }
